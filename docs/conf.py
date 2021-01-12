b306384400725e6fe22081eb98d8cb75be14bfd8
Sander Willems <swillems89@hotmail.com>
2021-01-12 10:54:54 +0100

    DOCS summary included for all modules

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,8 @@ author = 'Mann Labs, MPIB'
 # ones.
 extensions = [
     'sphinx.ext.napoleon',
-    "sphinx_rtd_theme"
+    "sphinx_rtd_theme",
+    'autodocsumm',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -52,3 +53,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+autodoc_default_options = {
+    'autosummary': False,
+}
--- docs/create_docs.sh
+++ docs/create_docs.sh
@@ -1,4 +1,4 @@
-rm -rf ../docs/build
+rm -rf build
 conda env remove -n alphatimsdocs
 conda create -n alphatimsdocs python=3.8 pip=20.2 -y
 # conda create -n alphatimsinstaller python=3.8
@@ -6,8 +6,9 @@ conda activate alphatimsdocs
 # call conda install git -y
 # call pip install 'git+https://github.com/MannLabs/alphatims.git#egg=alphatims[gui]' --use-feature=2020-resolver
 # brew install freetype
-pip install '../../.'
+pip install '../.'
 pip install sphinx_rtd_theme
 pip install sphinx
+pip install autodocsumm
 make html
 conda deactivate
