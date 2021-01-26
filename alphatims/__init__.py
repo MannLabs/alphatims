f66c4bcb15acf2cb0f41666cedfe708ead9506a4
Sander Willems <swillems89@hotmail.com>
2021-01-26 16:16:19 +0100

    DOCS version update

--- alphatims/__init__.py
+++ alphatims/__init__.py
@@ -2,7 +2,7 @@
 
 
 __project__ = "alphatims"
-__version__ = "0.0.210122"
+__version__ = "0.0.210126"
 __license__ = "MIT"
 __description__ = "A python package for Bruker TimsTOF raw data accession and visualization"
 __author__ = "Sander Willems"
