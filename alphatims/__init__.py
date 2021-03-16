d7f03dfe3a005eac41750d89cd7948e898f6c26d
Sander Willems <swillems89@hotmail.com>
2021-03-16 09:28:47 +0100

    CHORE version update

--- alphatims/__init__.py
+++ alphatims/__init__.py
@@ -2,7 +2,7 @@
 
 
 __project__ = "alphatims"
-__version__ = "0.0.210310"
+__version__ = "0.1.210317"
 __license__ = "Apache"
 __description__ = "A Python package for Bruker TimsTOF raw data accession and visualization"
 __author__ = "Sander Willems, Eugenia Voytik"
