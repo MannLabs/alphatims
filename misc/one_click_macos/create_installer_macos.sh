c48d4c1f15b01416647a9f439dcab73e10526289
Sander Willems <swillems89@hotmail.com>
2021-04-13 21:37:25 +0200

    CHORE prep macos installer for signing and notarization

--- misc/one_click_macos/create_installer_macos.sh
+++ misc/one_click_macos/create_installer_macos.sh
@@ -1,5 +1,6 @@
 rm -rf dist
 rm -rf build
+rm alphatims.pkg
 conda env remove -n alphatimsinstaller
 conda create -n alphatimsinstaller python=3.8 -y
 # conda create -n alphatimsinstaller python=3.8
@@ -12,8 +13,9 @@ rm -rf dist
 rm -rf build
 python setup.py sdist bdist_wheel
 cd misc/one_click_macos
-pip install "../../dist/alphatims-0.2.1-py3-none-any.whl[plotting]"
 pip install pyinstaller==4.2
+pip install "../../dist/alphatims-0.2.1-py3-none-any.whl[plotting]"
+conda list
 pyinstaller ../pyinstaller/alphatims.spec -y
 conda deactivate
 mkdir -p dist/alphatims/Contents/Resources
@@ -23,6 +25,23 @@ cp Info.plist dist/alphatims/Contents
 cp alphatims_terminal dist/alphatims/Contents/MacOS
 cp ../../LICENSE.txt Resources/LICENSE.txt
 cp ../alpha_logo.png Resources/alpha_logo.png
-pkgbuild --root dist/alphatims --identifier org.alphatims.0.2.1 --version 0.2.1 --install-location /Applications/AlphaTims.app --scripts scripts alphatims.pkg
-productbuild --distribution distribution.xml --resources Resources --package-path alphatims.pkg dist/alphatims_gui_installer_macos.pkg
-# productsign --sign "Developer ID Installer: John Doe" dist/alphatims_gui_installer_macos.pkg dist/alphatims_gui_installer_macos_signed.pkg
+
+if false; then
+  # https://scriptingosx.com/2019/09/notarize-a-command-line-tool/
+  for f in $(find dist/alphatims -name '*.so' -or -name '*.dylib'); do codesign --sign "Developer ID Application: Max-Planck-Gesellschaft zur Förderung der Wissenschaften e.V. (7QSY5527AQ)" $f; done
+  codesign --sign "Developer ID Application: Max-Planck-Gesellschaft zur Förderung der Wissenschaften e.V. (7QSY5527AQ)" dist/alphatims/Contents/MacOS/alphatims_gui --force --options=runtime --entitlements entitlements.xml
+  pkgbuild --root dist/alphatims --identifier de.mpg.biochem.alphatims.app --version 0.2.1 --install-location /Applications/AlphaTims.app --scripts scripts alphatims.pkg --sign "Developer ID Installer: Max-Planck-Gesellschaft zur Förderung der Wissenschaften e.V. (7QSY5527AQ)"
+  productbuild --distribution distribution.xml --resources Resources --package-path alphatims.pkg dist/alphatims_gui_installer_macos.pkg --sign "Developer ID Installer: Max-Planck-Gesellschaft zur Förderung der Wissenschaften e.V. (7QSY5527AQ)"
+  requestUUID=$(xcrun altool --notarize-app --primary-bundle-id "de.mpg.biochem.alphatims.app" --username "willems@biochem.mpg.de" --password "@keychain:Alphatims-develop" --asc-provider 7QSY5527AQ --file dist/alphatims_gui_installer_macos.pkg 2>&1 | awk '/RequestUUID/ { print $NF; }')
+  request_status="in progress"
+  while [[ "$request_status" == "in progress" ]]; do
+      echo "$request_status"
+      sleep 10
+      request_status=$(xcrun altool --notarization-info "$requestUUID" --username "willems@biochem.mpg.de" --password "@keychain:Alphatims-develop" | awk -F ': ' '/Status:/ { print $2; }' )
+  done
+  xcrun altool --notarization-info "$requestUUID" --username "willems@biochem.mpg.de" --password "@keychain:Alphatims-develop"
+  xcrun stapler staple dist/alphatims_gui_installer_macos.pkg
+else
+  pkgbuild --root dist/alphatims --identifier de.mpg.biochem.alphatims.app --version 0.2.1 --install-location /Applications/AlphaTims.app --scripts scripts alphatims.pkg
+  productbuild --distribution distribution.xml --resources Resources --package-path alphatims.pkg dist/alphatims_gui_installer_macos.pkg
+fi
