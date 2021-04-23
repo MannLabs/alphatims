45aec1e143af95c161acc0fa2314e8688c971f07
Sander Willems <swillems89@hotmail.com>
2021-04-23 22:12:34 +0200

    FIX pyinstaller interface params

--- misc/pyinstaller/alphatims_pyinstaller.py
+++ misc/pyinstaller/alphatims_pyinstaller.py
@@ -1,13 +1,7 @@
 if __name__ == "__main__":
     import alphatims.utils
-    log_file_name = alphatims.utils.set_logger(
-        log_file_name=alphatims.utils.INTERFACE_PARAMETERS["log_file"][
-            "default"
-        ]
-    )
-    threads = alphatims.utils.set_threads(
-        alphatims.utils.INTERFACE_PARAMETERS["threads"]["default"]
-    )
+    log_file_name = alphatims.utils.set_logger(log_file_name="")
+    threads = alphatims.utils.set_threads(-1)
     alphatims.utils.show_platform_info()
     alphatims.utils.show_python_info()
     alphatims.utils.check_github_version()
